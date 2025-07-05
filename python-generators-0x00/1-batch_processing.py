5e8187b984236f4d1b7283e3569d4b220afdf6cb
Martin Wainaina <122657979+codexoft-ke@users.noreply.github.com>
2025-07-05 22:24:57 +0300

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -13,8 +13,8 @@ def stream_users_in_batches(batch_size):
             cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset))
             rows = cursor.fetchall()
             if not rows:
-                break
-            yield rows  # ✅ yields one batch at a time
+                break  # 👈 exits loop instead of using `return`
+            yield rows  # ✅ yields a batch
             offset += batch_size
         cursor.close()
         connection.close()
@@ -24,7 +24,7 @@ def batch_processing(batch_size):
     """
     Processes and prints users over age 25 from batches.
     """
-    for batch in stream_users_in_batches(batch_size):  # loop 1
-        for user in batch:  # loop 2
-            if int(user['age']) > 25:
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
                 print(user)
